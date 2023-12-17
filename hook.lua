56415cb401bc3c11ca0c9002e26e1e8e7a6d3a83
ItsVoltz <29076540+ItsVoltz@users.noreply.github.com>
2023-12-17 12:47:30 -0500

    Now returns a closure instead of the object
    We return a closure wrapped with our __call method so if anyone wants to check the type and see that its a table instead of function to try and detect hooked functions it will now just show as a function

--- hook.lua
+++ hook.lua
@@ -1,18 +1,26 @@
+---@class Hook
+---@field fn function the original function
+---@field hook function the hooked function
 local Hook = {}
-
 Hook.__index = Hook
 
 function Hook:__call(...)
-  return self.hook(self.fn, ...)
+    return self.hook(self.fn, ...)
 end
 
+---Creates a new hook
+---@param original fun(...): any the original function
+---@param hook fun(original: fun(...): any, ...) the hooked function
+---@return fun(...)
 function Hook:New(original, hook)
-  local self = setmetatable({}, self)
-  
-  self.fn = original
-  self.hook = hook
-  
-  return self
+    local self = setmetatable({}, self)
+
+    self.fn = original
+    self.hook = hook
+
+    return function(...) -- We use a closure here instead of just directly returning the table with the __call metamethod because we don't want the hooekd function to show as a table, instead its still a function
+        return self(...)
+    end
 end
 
 return Hook
